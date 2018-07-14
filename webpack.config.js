fc773dc05c5d9f57af662d21ab428ca94c0159cc
derek <dereklimbus@gmail.com>
2018-07-14 11:57:06 +0200

    Add raw-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,21 @@ const path = require('path')
 
 module.exports = {
   entry: './src/index.js',
+
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.glsl$/,
+        use: 'raw-loader'
+      }
+    ]
+  },
+
   devServer: {
     contentBase: path.resolve(__dirname, 'dist')
   }
